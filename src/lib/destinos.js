89490126ef61ffce3f03447e414ed75672445672
Valfran <valfran-goncalves@hotmail.com>
2025-07-24 19:15:19 -0300

    feat: adicionando lista de destinos

---
+++ src/lib/destinos.js
@@ -0,0 +1,34 @@
+export const destinos = [
+  {
+    id: 1,
+    nome: "Campos do Jordão - São Paulo",
+    imagem:
+      "https://www.melhoresdestinos.com.br/wp-content/uploads/2021/06/campos-jordao-capa-2-1536x805.jpeg",
+    descricao:
+      "São Paulo é a capital do Brasil e a maior cidade do país. É conhecida por sua rica cultura, diversos parques e museus, e por ser uma das cidades mais populosas do mundo. São Paulo é um importante centro de negócios, financeiros e de tecnologia, além de ser um importante centro de investimento e de emprego.",
+  },
+  {
+    id: 2,
+    nome: "Rio de Janeiro",
+    imagem:
+      "https://www.melhoresdestinos.com.br/wp-content/uploads/2019/02/passagens-aereas-rio-de-janeiro-capa2019-01.jpg",
+    descricao:
+      "Rio de Janeiro é a segunda maior cidade do Brasil, com uma população de mais de 20 milhões de habitantes. É conhecida por sua rica cultura, diversos parques e museus, e por ser uma das cidades mais populosas do mundo. Rio de Janeiro é um importante centro de negócios, financeiros e de tecnologia, além de ser um importante centro de investimento e de emprego.",
+  },
+  {
+    id: 3,
+    nome: "Belo Horizonte",
+    imagem:
+      "https://media-cdn.tripadvisor.com/media/attractions-splice-spp-720x480/06/75/93/a0.jpg",
+    descricao:
+      "Belo Horizonte é a terceira maior cidade do Brasil, com uma população de mais de 10 milhões de habitantes. É conhecida por sua rica cultura, diversos parques e museus, e por ser uma das cidades mais populosas do mundo. Belo Horizonte é um importante centro de negócios, financeiros e de tecnologia, além de ser um importante centro de investimento e de emprego.",
+  },
+  {
+    id: 4,
+    nome: "Brasília",
+    imagem:
+      "https://imgmd.net/image/upload/c_limit%2Cw_3200/v1/guia/4470853.jpg",
+    descricao:
+      "Brasília é a capital do Brasil e a maior cidade do país. É conhecida por sua rica cultura, diversos parques e museus, e por ser uma das cidades mais populosas do mundo. Brasília é um importante centro de negócios, financeiros e de tecnologia, além de ser um importante centro de investimento e de emprego.",
+  },
+];

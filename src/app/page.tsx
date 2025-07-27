fc7d61d1ad986d77c8a8cce1ab8b735705848847
Valfran <valfran-goncalves@hotmail.com>
2025-07-26 21:53:04 -0300

    chore: ajustando a page principal como prévia da lista de destinos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import "@/styles/globals.css";
 import Grid from "./componentes/Grid";
 import { destinos } from "@/lib/destinos";
+import Title from "./componentes/Title";
+import Link from "next/link";
 
 export default function Home() {
+  const destaques = destinos.slice(0, 2);
   return (
     <>
-    <Grid destinos={destinos}/>
+    <Title title="Destinos em Alta"/>
+    <Grid destinos={destaques}/>
     </>
 
   );
